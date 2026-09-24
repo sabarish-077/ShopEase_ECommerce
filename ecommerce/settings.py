b8e9a42116418579fab3e629e48b7e3bf17338ed
Sabarish <sabarish2385@gmail.com>
2026-09-24 22:32:05 +0530

    Prepare ShopEase for deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -138,3 +138,6 @@ MAILERS = {
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -17,15 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("shop.urls")),
+
     path(
-    "accounts/login/",
-    auth_views.LoginView.as_view(
-        template_name="login.html"
+        "accounts/login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login"
     ),
-    name="login"
-),
+
+    path("", include("shop.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
